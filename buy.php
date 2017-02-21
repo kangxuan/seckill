c53f4a0560d518b5c6e04d472a7ecdbc253053c0
kangxuan <895560759@qq.com>
2017-02-21 14:03:08 +0800

    Update buy.php
    修改访问地址

--- buy.php
+++ buy.php
@@ -1,4 +1,4 @@
 <?php
 	
-	$url = 'http://192.168.16.73/Seckill/index.php?app=app&c=seckill&a=addQsec&gid=1&type=redis';
+	$url = 'http://127.0.0.1/Seckill/index.php?app=app&c=seckill&a=addQsec&gid=1&type=redis';
 	file_get_contents($url);
